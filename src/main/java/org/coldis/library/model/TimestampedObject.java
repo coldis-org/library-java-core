667dafd159d1223ef9a608adefe6a3c5584bd6e9
rvcoutinho <me@rvcoutinho.com>
2019-03-21 12:08:20 -0300

    - Creating generic model interfaces;
    Signed-off-by: workspace <workspace@workspace>

---
+++ src/main/java/org/coldis/library/model/TimestampedObject.java
@@ -0,0 +1,39 @@
+package org.coldis.library.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Time-stamped object.
+ */
+public interface TimestampedObject extends Serializable {
+
+	/**
+	 * Gets the object creation date/time.
+	 *
+	 * @return The object creation date/time.
+	 */
+	LocalDateTime getCreatedAt();
+
+	/**
+	 * Sets the object creation date/time.
+	 *
+	 * @param createdAt New object creation date/time.
+	 */
+	void setCreatedAt(final LocalDateTime createdAt);
+
+	/**
+	 * Gets the object last update date/time.
+	 *
+	 * @return The object last update date/time.
+	 */
+	LocalDateTime getUpdatedAt();
+
+	/**
+	 * Sets the object last update date/time.
+	 *
+	 * @param updatedAt New object last update date/time.
+	 */
+	void setUpdatedAt(final LocalDateTime updatedAt);
+
+}
