570e78c741cbd84c06c50349f5adee40719fe922
rvcoutinho <me@rvcoutinho.com>
2021-05-18 19:48:11 -0300

    - Creatng helper function;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/helper/DateTimeHelper.java
+++ src/main/java/org/coldis/library/helper/DateTimeHelper.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
@@ -17,12 +18,10 @@ public class DateTimeHelper {
 	/**
 	 * Date time formatter.
 	 */
-	public static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder().parseStrict()
-			.optionalStart().append(DateTimeFormatter.ISO_DATE).optionalEnd().optionalStart().appendLiteral('T')
-			.append(DateTimeFormatter.ISO_TIME).optionalEnd().parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-			.parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0).parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-			.parseDefaulting(ChronoField.NANO_OF_SECOND, 0).optionalStart().appendOffset("+HHMMss", "X").optionalEnd()
-			.toFormatter();
+	public static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder().parseStrict().optionalStart().append(DateTimeFormatter.ISO_DATE)
+			.optionalEnd().optionalStart().appendLiteral('T').append(DateTimeFormatter.ISO_TIME).optionalEnd().parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+			.parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0).parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0).parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
+			.optionalStart().appendOffset("+HHMMss", "X").optionalEnd().toFormatter();
 
 	/**
 	 * Clock to be used by the helper.
@@ -43,7 +42,8 @@ public class DateTimeHelper {
 	 *
 	 * @param clock Clock to be used by the helper.
 	 */
-	public static void setClock(final Clock clock) {
+	public static void setClock(
+			final Clock clock) {
 		DateTimeHelper.CLOCK = clock;
 	}
 
@@ -53,7 +53,8 @@ public class DateTimeHelper {
 	 * @param  offsets The offsets to adjust the clock.
 	 * @return         The original clock (before adjustments).
 	 */
-	public static Clock adjustClock(final Duration... offsets) {
+	public static Clock adjustClock(
+			final Duration... offsets) {
 		// Gets the original clock.
 		final Clock originalClock = DateTimeHelper.getClock();
 		Clock currentClock = originalClock;
@@ -95,6 +96,17 @@ public class DateTimeHelper {
 	 */
 	public static LocalTime getCurrentLocalTime() {
 		return LocalTime.now(DateTimeHelper.CLOCK);
+	}
+
+	/**
+	 * Converts the date to timestamp.
+	 *
+	 * @param  dateTime Date time.
+	 * @return          Date as timestamp.
+	 */
+	public static Long toTimestamp(
+			final LocalDateTime dateTime) {
+		return (dateTime == null ? null : dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
 	}
 
 }
