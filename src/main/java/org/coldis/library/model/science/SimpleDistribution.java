6dbdb0f29a935989def58266bd019a7120f99919
rvcoutinho <me@rvcoutinho.com>
2022-03-13 10:57:05 -0300

    - Adding distribution groups to constructor;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/model/science/SimpleDistribution.java
+++ src/main/java/org/coldis/library/model/science/SimpleDistribution.java
@@ -1,5 +1,9 @@
 package org.coldis.library.model.science;
 
+import java.util.List;
+
+import org.coldis.library.model.Primaryable;
+
 /**
  * Simple distribution.
  */
@@ -9,5 +13,23 @@ public class SimpleDistribution extends AbstractDistribution {
 	 * Serial.
 	 */
 	private static final long serialVersionUID = 9101491398722597248L;
+
+	/**
+	 * No arguments constructor.
+	 */
+	public SimpleDistribution() {
+		super();
+	}
+
+	/**
+	 * Default constructor.
+	 *
+	 * @param groups Groups.
+	 */
+	public SimpleDistribution(final List<DistributionGroup> groups) {
+		super();
+		this.setGroups(groups);
+		Primaryable.autoAssignPrimary(this.getGroups());
+	}
 
 }
