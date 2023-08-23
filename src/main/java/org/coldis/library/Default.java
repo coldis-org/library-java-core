ea1e97c2450f46e2d12838f0769fb91c2749078b
rvcoutinho <me@rvcoutinho.com>
2023-08-23 14:00:11 -0300

    - Adding configuration;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

---
+++ src/main/java/org/coldis/library/Default.java
@@ -0,0 +1,13 @@
+package org.coldis.library;
+
+/**
+ * Defaults.
+ */
+public class Configuration {
+
+	/**
+	 * Base package.
+	 */
+	public static final String BASE_PACKAGE = "org.coldis.library";
+
+}
