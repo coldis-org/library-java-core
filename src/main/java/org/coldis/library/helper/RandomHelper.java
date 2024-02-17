73399798d2173b31b8ea574bec29a5da215981ef
rvcoutinho <me@rvcoutinho.com>
2024-02-16 21:38:05 -0300

    - Improving random helper;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/helper/RandomHelper.java
+++ src/main/java/org/coldis/library/helper/RandomHelper.java
@@ -1,6 +1,10 @@
 package org.coldis.library.helper;
 
+import java.security.SecureRandom;
+import java.util.Collection;
 import java.util.Random;
+
+import org.apache.commons.collections4.CollectionUtils;
 
 /**
  * Random content helper.
@@ -13,11 +17,6 @@ public class RandomHelper {
 	public static final String CLEAR_APLHANUMERIC_CHARS = "234679ACDEFGHJKMNPQRTVWXYZ";
 
 	/**
-	 * Numbers.
-	 */
-	public static final String NUMBERS = "0123456789";
-
-	/**
 	 * Characters.
 	 */
 	public static final String CHARS = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ234567890";
@@ -25,7 +24,7 @@ public class RandomHelper {
 	/**
 	 * Random.
 	 */
-	private static final Random RANDOM = new Random();
+	public static final Random RANDOM = new SecureRandom();
 
 	/**
 	 * Gets some random string.
@@ -34,7 +33,9 @@ public class RandomHelper {
 	 * @param  possibleCharacters Possible characters to be generated.
 	 * @return                    Some random string.
 	 */
-	public static String getRandomString(final Integer numberOfCharacters, final String possibleCharacters) {
+	public static String getRandomString(
+			final Integer numberOfCharacters,
+			final String possibleCharacters) {
 		// Random string.
 		final StringBuffer randomCode = new StringBuffer();
 		// For the desired number of characters.
@@ -47,13 +48,37 @@ public class RandomHelper {
 	}
 
 	/**
-	 * Gets some random number.
+	 * Gets a positive random number (integer).
 	 *
-	 * @param  numberOfNumbers Number of numbers to be generated.
-	 * @return                 Some random number.
+	 * @param  maximum Maximum value.
+	 * @return         a positive random number (integer).
 	 */
-	public static String getRandomNumber(final Integer numberOfNumbers) {
-		return RandomHelper.getRandomString(numberOfNumbers, RandomHelper.NUMBERS);
+	public Integer getPositiveRandomInteger(
+			final Integer maximum) {
+		return Math.abs(maximum == null ? RandomHelper.RANDOM.nextInt() : RandomHelper.RANDOM.nextInt(maximum));
+	}
+
+	/**
+	 * Gets a positive random number (long).
+	 *
+	 * @param  maximum Maximum value.
+	 * @return         a positive random number (long).
+	 */
+	public Long getPositiveRandomLong(
+			final Long maximum) {
+		return Math.abs(maximum == null ? RandomHelper.RANDOM.nextLong() : RandomHelper.RANDOM.nextLong(maximum));
+	}
+
+	/**
+	 * Gets a random item from a collection.
+	 *
+	 * @param  <Type> The type of the items.
+	 * @param  items  The collection of items.
+	 * @return        A random item from the collection.C
+	 */
+	public static <Type> Type getRandomItem(
+			final Collection<Type> items) {
+		return (CollectionUtils.isEmpty(items) ? null : (Type) items.stream().skip(RandomHelper.RANDOM.nextInt(items.size())).findFirst().get());
 	}
 
 }
